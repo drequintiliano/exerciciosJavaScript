2b9db0fc1b9f5674d4dcd93ed4d40ef29379fa75
André Quintiliano Filho <drequintiliano@gmail.com>
2020-01-25 16:37:17 -0300

    Update exer.js

--- exer.js
+++ exer.js
@@ -1,26 +1,12 @@
-const cartoes = [{
-        id: 2,
-        bandeira: 'Mastercard (2017)'
-    },
-    {
-        id: 3,
-        bandeira: 'American Express, Diners'
-    },
-    {
-        id: 4,
-        bandeira: 'Visa'
-    },
-    {
-        id: 5,
-        bandeira: 'Mastercard, Elo'
-    },
-    {
-        id: 6,
-        bandeira: 'Elo, Discover'
-    }
+const cartoes = [
+    { id: 2, bandeira: 'Mastercard (2017)' },
+    { id: 3, bandeira: 'American Express, Diners' },
+    { id: 4, bandeira: 'Visa' },
+    { id: 5, bandeira: 'Mastercard, Elo' },
+    { id: 6, bandeira: 'Elo, Discover' }
 ]
 
-let cartao = '51300232309094343'
+let cartao = '5300232309094343'
 
 const identificaCartao = c => c.id === parseInt(cartao.substr(0, 1))
 const bandeira = b => b.bandeira
@@ -28,20 +14,6 @@ const validandoCartao = cartoes.filter(identificaCartao).length === 0 ? '' : car
 const resultado = validandoCartao > 0 ? cartoes.filter(identificaCartao).reduce(bandeira) : validandoCartao
 
 console.log(resultado.bandeira || 'Digito(s) Inválido(s)');
-
-
-// if (validandoCartao > 0) {
-
-//     return console.log(resultado);
-// } else {
-//     return console.log();
-// }
-
-
-// console.log(card);
-// console.log();
-// console.log(validandoCartao)
-// console.log(resultado);
 
 
 //console.log('exemplo: ' +  card.substr(0, 1)); // exemplo para pegar 1º digito de uma string
